3234c0ff8c0ae91fe42893cdebb77d31c2ac418b
jimmycababa <jimmycababa@gmail.com>
2021-09-12 23:02:19 -0500

    added sign in and out

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,8 +5,9 @@ class RegistrationsController < ApplicationController
 # saving the user info to the database through the create method
     def create
         @user = User.new(user_params)
-        if @user.dave
-            redirect_to root_path, notice: "Created Successfully"
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to root_path, notice: "Account Created Successfully"
         else
             # rendering the registration views new.html.erb
             render :new
